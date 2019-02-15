de3c458dd59ade45497a00d2a8dcd2e7895e90a7
nightcandle <instkffff@gmail.com>
2019-02-15 12:17:17 +0800

    .

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const esi = require('eve-swagger')
 const mysql = require('mysql')
+const combijson = require('combijson')
 
-const connection = mysql.createConnection({
+//whitelist group
+whitelistGroup = ['1','2','4']
+//whitelist alliance
+whitelistAlliance = ['99003581','99006828','99007498']
+
+const connection = mysql.createPool({
+	connectionLimit : 1000,
 	host : '199.247.16.189',
 	user : 'admin',
 	password : '123456',
@@ -19,14 +26,12 @@ const esi2 = esi({
 
 })
 
-function users(){
-	//sql connect
-	connection.connect()
-	let userList = 'SELECT * FROM users'
-
+// 'SELECT * FROM users'
+// 'SELECT * FROM group_user'
+function Query(condition){	
 	return new Promise(
 		function(resolve,reject){
-			connection.query(userList,function(err,result){
+			connection.query(condition,function(err,result){
 				if(err){
 					reject(err)
 				}
@@ -34,29 +39,82 @@ function users(){
 			})
 		}
 	)
-	connection.end()
 }
 
-let roleSync = users().then(result =>{
-	for ( i=0; i<result.length; i++){
-		let charJson = JSON.stringify(eval(result[i]))
-		let char = JSON.parse(charJson)
-		let charID = char.character_id
-		let ID = char.id
-		if( charID === null){
+// 'UPDATE group_user SET group_id = ? WHERE user_id = ?';
+function Assign(condition,user_id,role){
+	return new Promise(
+		function(resolve,reject){
+			let modSqlParams = [role,user_id]
+			connection.query(condition,modSqlParams,function(err,result){
+				if(err){
+					reject(err)
+				}
+				resolve('sync successfully')
+			})
+		}
+	)
+}
+
+async function Sync(){
+	
+	let users = await Query('SELECT * FROM users')
+	let group_user = await Query('SELECT * FROM group_user')
+
+	let userInfoCombi = combijson([users,group_user])
+	let userInfojson = JSON.stringify(eval(userInfoCombi))
+	let userInfo = JSON.parse(userInfojson)
+
+	for(i=0;i<users.length;i++){
+		let charjson = JSON.stringify(eval(userInfo[i]))
+		let char = JSON.parse(charjson)
+		let character_id = char.character_id
+		let id = char.id
+		let group_id = char.group_id
+		let user_id = char.user_id
+		if(character_id === null){
 			console.log('this is admin')
 		}else{
-			esi2.characters(charID).info().then(info =>{
+			esi2.characters(character_id).info().then(info =>{
 				let charInfoJson = JSON.stringify(eval(info))
 				let charInfo = JSON.parse(charInfoJson)
 				let alliance_id = charInfo.alliance_id
 				let corporation_id = charInfo.corporation_id
-				console.log(corporation_id)
-			}).catch(error => {
+				int1 = whitelistAlliance.indexOf(alliance_id.toString())
+				int2 = whitelistGroup.indexOf(group_id.toString())
+				if(int2 >= 0){
+					console.log('this role assign manually')
+				}else if(int1 >= 0){
+					Assign('UPDATE group_user SET group_id = ? WHERE user_id = ?',user_id,5).then(result => {
+						console.log(result)
+					}).catch(error =>{
+						console.log(error)
+					})
+				}else{
+					Assign('UPDATE group_user SET user_id = ? WHERE user_id = ?' ,user_id,3).then(result => {
+						console.log(result)
+					}).catch(error =>{
+						console.log(error)
+					})
+				}
+			}).catch(error =>{
 				console.log(error)
 			})
 		}
 	}
-}).catch(error => {
-	console.log(error)
+	
+}
+
+
+
+setInterval(function(){
+	Sync()
+},600000)
+
+//ignore error
+process.on("uncatchException", function(e) {
+    console.log(e);
+    process.exit(1);
 })
+
+
